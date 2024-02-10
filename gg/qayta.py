ce87e9b23ef8b1f730be440c67315eb5bea85925
Yellow4554Mouse <yellowmause@gmail.com>
2024-02-10 15:52:36 +0500

    asdjbasd

--- gg/qayta.py
+++ gg/qayta.py
@@ -1,1 +1,11 @@
-print ("haaiodjaf")
+name = "kris"
+password = "kajfk"
+age = 4000
+if name != "kris":
+    print("you are not elis")
+elif age < 12 :
+    print("are you krid")
+elif age > 3000:
+    print("unlike you kris is not an undead , imortal vamper")
+elif age > 100:
+    print("you are not karl grannie") 
